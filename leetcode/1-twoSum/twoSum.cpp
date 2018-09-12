0e75cb0c3c9e37529c6fce494d3846851fe9da8d
vacingfang <vacingfang@tencent.com>
2018-09-13 00:57:21 +0800

    1-改正原有方法不能解决重复数字的问题 2-新增完全基于Map的简单算法

--- leetcode/1-twoSum/twoSum.cpp
+++ leetcode/1-twoSum/twoSum.cpp
@@ -8,15 +8,46 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        auto ans = vector<int>();
+        auto num2Index = map<int, int>();
+        for (unsigned i = 0; i < nums.size(); i++) {
+            int min = target - nums[i];
+            auto it1 = num2Index.find(nums[i]); // 利用map的便利性
+            auto it2 = num2Index.find(min);
+            if(it2 != num2Index.end()) {        // find complement in map
+                ans.push_back(it2->second);
+                ans.push_back(i);
+                return ans;
+            }
+            else if(it1 != num2Index.end()) {  // keep first ind when repeat
+                continue;   
+            }
+            else {
+                num2Index[nums[i]] = i; // add ind
+            }
+        }
+
+        return ans;
+    }
+
+    vector<int> twoSumLow(vector<int>& nums, int target) {
+        auto ans = vector<int>();
         auto num2Index = map<int, int>();
         for (int i = 0; i < nums.size(); i++) {
+            auto it = num2Index.find(nums[i]);
+            if(it != num2Index.end()) {     // 数字重复 
+                if(nums[i] * 2 == target) {
+                    ans.push_back(it->second);
+                    ans.push_back(i);
+                    return ans;
+                }
+                continue;       // 保留第一个出现的位置
+            }
             num2Index[nums[i]] = i;
         }
-        auto ans = vector<int>();
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // sort本身复杂度较高
         bool found = false;
-        for(int i=0, j=nums.size()-1; i < j; i++) {
-            int min = target - nums[i];
+        for(int i=0, j=nums.size()-1; i < j; i++) { // 利用数组的有序性
             for(/* NULL */; i < j; j--) {
                 if(nums[j] > min) {
                     continue;
@@ -45,9 +76,9 @@ public:
 int main()
 {
     auto so = Solution();
-    auto nums = vector<int>{5, 6, 2, 2, 7, 11, 15};
+    auto nums = vector<int>{5, 5, 2, 2, 7, 11, 15};
     auto numsCpy = nums;
-    auto target = 4;
+    auto target = 10;
 
     vector<int> ans = so.twoSum(numsCpy, target);
     for (auto &i : ans) {
